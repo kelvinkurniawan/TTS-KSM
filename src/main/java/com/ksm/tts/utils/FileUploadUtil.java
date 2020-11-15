b30fe0ea9e3ddccb60d6b4b5ac6963d26774431b
Kelvin Kurniawan O <tepinnko@gmail.com>
2020-11-16 05:43:18 +0700

    Add default avatar and fixing register page

--- src/main/java/com/ksm/tts/utils/FileUploadUtil.java
+++ src/main/java/com/ksm/tts/utils/FileUploadUtil.java
@@ -33,11 +33,14 @@ public class FileUploadUtil {
         try {
             Path file = root.resolve(filename);
             Resource resource = new UrlResource(file.toUri());
+            
+            Path defaultFile = root.resolve("avatar.png");
+            Resource defaualResource = new UrlResource(defaultFile.toUri());
 
             if (resource.exists() || resource.isReadable()) {
                 return resource;
             } else {
-                throw new RuntimeException("Could not read the file!");
+                return defaualResource;
             }
         } catch (MalformedURLException e) {
             throw new RuntimeException("Error: " + e.getMessage());
